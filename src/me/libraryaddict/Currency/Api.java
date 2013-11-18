d63529d20f5cc34ceeaf9243019d5d504ece49c7
LimeByte <limebyte@twf.co.nz>
2013-11-18 15:03:34 +1300

    Format source

--- src/me/libraryaddict/Currency/Api.java
+++ src/me/libraryaddict/Currency/Api.java
@@ -3,44 +3,33 @@ package me.libraryaddict.Currency;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class Api
-{
-	CurrencyMain main;
-	
-	Api(CurrencyMain cur)
-	{
-		this.main = cur;
-	}
-	
-	public void pay(String player, int amount, boolean message)
-	{
-		if (CurrencyMain.balance.contains(player))
-			CurrencyMain.balance.put(player, Integer.valueOf(((Integer) CurrencyMain.balance.get(player)).intValue() - amount));
-		if (message)
-			CurrencyMain.transfers.add(new Transfer(null, player, Math.abs(amount), Transfer.MONEY_GIVE));
-		else
-			CurrencyMain.transfers.add(new Transfer(null, player, Math.abs(amount), Transfer.SILENT));
-	}
-	
-	public void pay(String sender, String receiver, int amount, boolean message)
-	{
-		if (CurrencyMain.balance.contains(sender))
-			CurrencyMain.balance.put(sender, Integer.valueOf(((Integer) CurrencyMain.balance.get(sender)).intValue() - amount));
-		if (message)
-			CurrencyMain.transfers.add(new Transfer(sender, receiver, Math.abs(amount), Transfer.PLAYER_PAYMENT));
-		else
-			CurrencyMain.transfers.add(new Transfer(sender, receiver, Math.abs(amount), Transfer.SILENT));
-	}
-	
-	public void withdraw(String player, int amount)
-	{
-		if (CurrencyMain.balance.contains(player))
-			CurrencyMain.balance.put(player, Integer.valueOf(((Integer) CurrencyMain.balance.get(player)).intValue() - amount));
-		CurrencyMain.transfers.add(new Transfer(player, null, Math.abs(amount), Transfer.SILENT));
-	}
-	
-	public void refresh(String player)
-	{
-		CurrencyMain.refreshers.add(player);
-	}
+public class Api {
+    CurrencyMain main;
+
+    Api(CurrencyMain cur) {
+        this.main = cur;
+    }
+
+    public void pay(String player, int amount, boolean message) {
+        if (CurrencyMain.balance.contains(player)) CurrencyMain.balance.put(player, Integer.valueOf(((Integer) CurrencyMain.balance.get(player)).intValue() - amount));
+        if (message) CurrencyMain.transfers.add(new Transfer(null, player, Math.abs(amount), Transfer.MONEY_GIVE));
+        else
+            CurrencyMain.transfers.add(new Transfer(null, player, Math.abs(amount), Transfer.SILENT));
+    }
+
+    public void pay(String sender, String receiver, int amount, boolean message) {
+        if (CurrencyMain.balance.contains(sender)) CurrencyMain.balance.put(sender, Integer.valueOf(((Integer) CurrencyMain.balance.get(sender)).intValue() - amount));
+        if (message) CurrencyMain.transfers.add(new Transfer(sender, receiver, Math.abs(amount), Transfer.PLAYER_PAYMENT));
+        else
+            CurrencyMain.transfers.add(new Transfer(sender, receiver, Math.abs(amount), Transfer.SILENT));
+    }
+
+    public void withdraw(String player, int amount) {
+        if (CurrencyMain.balance.contains(player)) CurrencyMain.balance.put(player, Integer.valueOf(((Integer) CurrencyMain.balance.get(player)).intValue() - amount));
+        CurrencyMain.transfers.add(new Transfer(player, null, Math.abs(amount), Transfer.SILENT));
+    }
+
+    public void refresh(String player) {
+        CurrencyMain.refreshers.add(player);
+    }
 }
--- src/me/libraryaddict/Currency/MoneySaveThread.java
+++ src/me/libraryaddict/Currency/MoneySaveThread.java
@@ -11,114 +11,88 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitScheduler;
 
-public class MoneySaveThread extends Thread
-{
-	CurrencyMain plugin;
-	public Connection con = null;
-	
-	public MoneySaveThread(CurrencyMain Money)
-	{
-		this.plugin = Money;
-	}
-	
-	public void SQLdisconnect()
-	{
-		try
-		{
-			System.out.println("[MoneySaveThread] Disconnecting from MySQL database...");
-			this.con.close();
-		} catch (SQLException ex)
-		{
-			System.err.println("[MoneySaveThread] Error while closing the connection...");
-		} catch (NullPointerException ex)
-		{
-			System.err.println("[MoneySaveThread] Error while closing the connection...");
-		}
-	}
-	
-	public void SQLconnect()
-	{
-		try
-		{
-			System.out.println("[MoneySaveThread] Connecting to MySQL database...");
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			String conn = "jdbc:mysql://" + this.plugin.SQL_HOST + "/" + this.plugin.SQL_DATA;
-			this.con = DriverManager.getConnection(conn, this.plugin.SQL_USER, this.plugin.SQL_PASS);
-		} catch (ClassNotFoundException ex)
-		{
-			System.err.println("[MoneySaveThread] No MySQL driver found!");
-		} catch (SQLException ex)
-		{
-			System.err.println("[MoneySaveThread] Error while fetching MySQL connection!");
-		} catch (Exception ex)
-		{
-			System.err.println("[MoneySaveThread] Unknown error while fetchting MySQL connection.");
-		}
-	}
-	
-	public void run()
-	{
-		System.out.println("Money Save Thread started");
-		SQLconnect();
-		while (true)
-		{
-			if (CurrencyMain.transfers.peek() != null)
-			{
-				final Transfer transfer = (Transfer) CurrencyMain.transfers.poll();
-				String[] name =
-				{transfer.getSender(), transfer.getReceiver()};
-				Integer[] amount =
-				{Integer.valueOf(-transfer.getAmount()), Integer.valueOf(transfer.getAmount())};
-				transfer.setStatus(true);
-				for (int n = 0; n < 2; n++)
-					if (name[n] != null)
-					{
-						String statement = "";
-						try
-						{
-							Statement stamt = this.con.createStatement();
-							statement = "SELECT * FROM `AGMoney` WHERE `Name` = '" + name[n] + "' ;";
-							ResultSet r = stamt.executeQuery(statement);
-							r.last();
-							int money = r.getInt("Money");
-							r.close();
-							CurrencyMain.write(" Updating " + name[n] + "'s money, Setting it from " + money + " to " + (money + amount[n].intValue()) + ", Transfer type: " + transfer.getType());
-							System.out.println("Updating " + name[n] + "'s money, Setting it from " + money + " to " + (money + amount[n].intValue()));
-							statement = "UPDATE AGMoney SET Money='" + (money + amount[n].intValue()) + "' WHERE `Name` = '" + name[n] + "' ;";
-							stamt.executeUpdate(statement);
-							stamt.close();
-							CurrencyMain.balance.put(name[n], Integer.valueOf(money + amount[n].intValue()));
-						} catch (SQLException ex)
-						{
-							transfer.setStatus(false);
-							transfer.setError(ex.getMessage());
-							System.err.println("[MoneySaveThread] Error while doing statement: " + statement);
-							System.err.println("[MoneySaveThread] MySql error: " + ex.getMessage());
-						} catch (NullPointerException ex)
-						{
-							transfer.setStatus(false);
-							transfer.setError(ex.getMessage());
-							System.err.println("[MoneySaveThread] Error while doing statement: " + statement);
-							System.err.println("[MoneySaveThread] Error while performing a query. (NullPointerException)");
-						}
-					}
-				if (this.plugin.isEnabled())
-					Bukkit.getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable()
-					{
-						public void run()
-						{
-							MoneySaveThread.this.plugin.transferComplete(transfer);
-						}
-					});
-			}
-			if (CurrencyMain.transfers.peek() == null)
-				try
-				{
-					Thread.currentThread();
-					Thread.sleep(1000L);
-				} catch (InterruptedException localInterruptedException)
-				{
-				}
-		}
-	}
+public class MoneySaveThread extends Thread {
+    CurrencyMain plugin;
+    public Connection con = null;
+
+    public MoneySaveThread(CurrencyMain Money) {
+        this.plugin = Money;
+    }
+
+    public void SQLdisconnect() {
+        try {
+            System.out.println("[MoneySaveThread] Disconnecting from MySQL database...");
+            this.con.close();
+        } catch (SQLException ex) {
+            System.err.println("[MoneySaveThread] Error while closing the connection...");
+        } catch (NullPointerException ex) {
+            System.err.println("[MoneySaveThread] Error while closing the connection...");
+        }
+    }
+
+    public void SQLconnect() {
+        try {
+            System.out.println("[MoneySaveThread] Connecting to MySQL database...");
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String conn = "jdbc:mysql://" + this.plugin.SQL_HOST + "/" + this.plugin.SQL_DATA;
+            this.con = DriverManager.getConnection(conn, this.plugin.SQL_USER, this.plugin.SQL_PASS);
+        } catch (ClassNotFoundException ex) {
+            System.err.println("[MoneySaveThread] No MySQL driver found!");
+        } catch (SQLException ex) {
+            System.err.println("[MoneySaveThread] Error while fetching MySQL connection!");
+        } catch (Exception ex) {
+            System.err.println("[MoneySaveThread] Unknown error while fetchting MySQL connection.");
+        }
+    }
+
+    public void run() {
+        System.out.println("Money Save Thread started");
+        SQLconnect();
+        while (true) {
+            if (CurrencyMain.transfers.peek() != null) {
+                final Transfer transfer = (Transfer) CurrencyMain.transfers.poll();
+                String[] name = { transfer.getSender(), transfer.getReceiver() };
+                Integer[] amount = { Integer.valueOf(-transfer.getAmount()), Integer.valueOf(transfer.getAmount()) };
+                transfer.setStatus(true);
+                for (int n = 0; n < 2; n++)
+                    if (name[n] != null) {
+                        String statement = "";
+                        try {
+                            Statement stamt = this.con.createStatement();
+                            statement = "SELECT * FROM `AGMoney` WHERE `Name` = '" + name[n] + "' ;";
+                            ResultSet r = stamt.executeQuery(statement);
+                            r.last();
+                            int money = r.getInt("Money");
+                            r.close();
+                            CurrencyMain.write(" Updating " + name[n] + "'s money, Setting it from " + money + " to " + (money + amount[n].intValue()) + ", Transfer type: " + transfer.getType());
+                            System.out.println("Updating " + name[n] + "'s money, Setting it from " + money + " to " + (money + amount[n].intValue()));
+                            statement = "UPDATE AGMoney SET Money='" + (money + amount[n].intValue()) + "' WHERE `Name` = '" + name[n] + "' ;";
+                            stamt.executeUpdate(statement);
+                            stamt.close();
+                            CurrencyMain.balance.put(name[n], Integer.valueOf(money + amount[n].intValue()));
+                        } catch (SQLException ex) {
+                            transfer.setStatus(false);
+                            transfer.setError(ex.getMessage());
+                            System.err.println("[MoneySaveThread] Error while doing statement: " + statement);
+                            System.err.println("[MoneySaveThread] MySql error: " + ex.getMessage());
+                        } catch (NullPointerException ex) {
+                            transfer.setStatus(false);
+                            transfer.setError(ex.getMessage());
+                            System.err.println("[MoneySaveThread] Error while doing statement: " + statement);
+                            System.err.println("[MoneySaveThread] Error while performing a query. (NullPointerException)");
+                        }
+                    }
+                if (this.plugin.isEnabled()) Bukkit.getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable() {
+                    public void run() {
+                        MoneySaveThread.this.plugin.transferComplete(transfer);
+                    }
+                });
+            }
+            if (CurrencyMain.transfers.peek() == null) try {
+                Thread.currentThread();
+                Thread.sleep(1000L);
+            } catch (InterruptedException localInterruptedException) {
+            }
+        }
+    }
 }
